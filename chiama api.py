c70e5c82ae80ce520c33c1443b75353da11b29dc
sara-sarhan <75019592+sara-sarhan@users.noreply.github.com>
2022-05-17 12:22:35 +0200

    Add files via upload

--- chiama api.py
+++ chiama api.py
@@ -1,5 +1,6 @@
 import requests
 
+## chima api lancia
 url = "http://127.0.0.1:8051//fileupload"
 
 payload={}
@@ -10,5 +11,25 @@ files=[
 headers = {}
 
 response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print(response.text)
+print(response.status_code)
+
+## chima api start 
+url = "http://127.0.0.1:8051/services/start"
+
+payload={}
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+url = "http://127.0.0.1:8051/services/healtcheck/isalive"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
